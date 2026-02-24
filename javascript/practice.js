f155cd3a0371cdcdf35274ed55e975909d5642c5
meghhakathait <meghakathait847@gmail.com>
2026-02-24 23:49:52 +0530

    Initial commit

---
+++ javascript/practice.js
@@ -0,0 +1,208 @@
+// let val1 = Number(prompt("enter 1st number"));
+// let val2 = Number(prompt("enter 2st number"));
+
+// // let result = val1 % val2;
+
+// // console.log(result);
+
+// alert(val1 % val2);
+
+
+
+// let sub1 = 80;
+// let sub2 = 49;
+// let sub3 = 74;
+// let sub4 = 90;
+// let sub5 = 61;
+
+// let result = ((sub1 + sub2 + sub3 + sub4 + sub5)/500)*100;
+
+// console.log(result);
+
+
+// let age = Number(prompt("enter ur age"));
+
+// if (age>= 60){
+//     console.log("discount available")
+// }
+
+// else{
+//     console.log("discount not available")
+// }
+
+
+// let day = prompt("enter a day")
+
+// // if ((day === "sunday") || (day === "saturday")){
+// if ((day == "sunday") || (day == "saturday")){
+//     console.log(" its a weekend")
+// }
+
+// else{
+//     console.log(" its not a weekend")
+// }
+
+
+
+
+// let weigth = Number(prompt('enter your weigth'));
+// let heigth = Number(prompt('enter your heigth'));
+
+// let BMI = (weigth / (heigth*heigth))*10000;
+
+// if (BMI < 18.5){
+//     console.log("underweigth")
+// }
+
+// else if (BMI >= 18.5 && BMI <= 24.9){
+//     console.log("normal weigth")
+// }
+
+
+// else if ((BMI >= 25)&&
+// (BMI <= 29.9)){
+//     console.log("overweigth")
+// }
+
+// else if (BMI >=30){
+//     console.log("obese")
+// }
+
+// else{
+//     console.log("invalid input")
+// }
+
+// OR
+
+// if (BMI < 18.5){
+//     console.log("underweigth")
+// }
+
+// else if (BMI < 24.9){
+//     console.log("normal weigth")
+// }
+
+// else if (BMI < 29.9){
+//     console.log("overweigth")
+// }
+
+// else if (BMI >=30){
+//     console.log("obese")
+// }
+
+// else{
+//     console.log("invalid input")
+// }
+
+
+
+
+
+
+// let unit = Number(prompt("enter number of units"));
+// let bill;
+
+// if (unit <= 100){
+//     bill = unit*8
+//     console.log(bill)
+// }
+
+// else if (unit <300){
+//     bill = unit*5
+//     console.log(bill)
+// }
+
+// else if (unit <500){
+//     bill = unit*3
+//     console.log(bill)
+// }
+
+// else if (unit >500){
+//     bill = unit*2
+//     console.log(bill)
+// }
+
+// else{
+//     console.log("invalid input")
+// }
+
+
+
+// let unit = Number(prompt("enter number of units"));
+// let bill;
+
+// if (unit <= 100){
+//     bill = unit*8
+//     console.log(bill)
+// }
+
+// else if (unit <=300){
+//     bill = (100*8) + ((unit-100)*5)
+//     console.log(`your bill amount id : &{bill}`);
+// }
+
+// else if (unit <=500){
+//     bill = (100*8) + (200*5) + ((unit-100)*3)
+//     console.log(`your bill amount id : &{bill}`);
+// }
+
+// else if (unit >500){
+//     bill = (100*8) + (200*5) + (200*3) + ((unit-100)*2)
+//     console.log(`your bill amount id : &{bill}`);
+// }
+
+// else{
+//     console.log("invalid input")
+// }
+
+
+
+// let number = Number(prompt("enter number"))
+
+// for (let num=1; num <=number; num++){
+//     console.log(num);
+// }
+
+
+// let sum= 0;
+
+// for (let num =1; num<=5; num++){
+//     sum += num;
+//     console.log(sum);
+// }
+// console.log(` the sum of first 5 num is: ${sum} `);
+
+
+// let num = Number(prompt("enter number"));
+
+// let evenNum=2;
+
+// while (evenNum<=num){
+//     if(evenNum=evenNum/2 == 0;)
+//     console.log(evenNum) 
+//     evenNum++
+// }
+
+
+// let num = Number(prompt("enter number"));
+
+// let sum=0;
+
+// for (let evenNum=1; evenNum<=num; evenNum++){
+//    if(evenNum%2 ==0){
+//        sum += evenNum;
+//    }
+   
+// }
+// console.log(sum);
+
+
+// let sum=0;
+
+// for (let evenNum=1; evenNum<=num; evenNum++){
+//    if(evenNum%2 ==0){
+//        sum += evenNum;
+//    }
+   
+// }
+
